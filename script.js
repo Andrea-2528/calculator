6dd24931eb3851e0155de92a124957bfee1a18da
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2024-11-29 00:28:21 +0100

    Add logic for almost all buttons except C and +/-

--- script.js
+++ script.js
@@ -14,21 +14,88 @@ function divide (a, b) {
     return a/b;
 };
 
-let firstNumber;
-let secondNumber;
-let operator;     // Should accept +, -, *, /
-
-function operate (a, b, symbol) {
+function operate (a, b, op) {
+    flag = true;
     let result;
-    switch(symbol){
+    b = parseFloat(displayInput.textContent);
+    // displayLog.textContent= displayLog.textContent.concat(displayInput.textContent) + "=";
+    switch(op){
         case "+":
-            return result = add(a, b);
+            result = Math.round((add(a, b) * 100) * (1 + Number.EPSILON)) / 100;
+            if(result>=100000000||result<=-100000000){
+                return displayInput.textContent = result.toExponential(4);
+            }else{
+                return displayInput.textContent = result;
+            }
         case "-":
-            return result = subtract(a, b);
-        case "*":
-            return result = multiply(a, b);
+            result = Math.round((subtract(a, b) * 100) * (1 + Number.EPSILON)) / 100;
+            if(result>=100000000||result<=-100000000){
+                return displayInput.textContent = result.toExponential(4);
+            }else{
+                return displayInput.textContent = result;
+            }
+        case "X":
+            result = Math.round((multiply(a, b) * 100) * (1 + Number.EPSILON)) / 100;
+            if(result>=100000000||result<=-100000000){
+                return displayInput.textContent = result.toExponential(4);
+            }else{
+                return displayInput.textContent = result;
+            }
         case "/":
-            return result = divide(a, b);
+            result = Math.round((divide(a, b) * 100) * (1 + Number.EPSILON)) / 100;
+            if(result>=100000000||result<=-100000000){
+                return displayInput.textContent = result.toExponential(4);
+            }else{
+                return displayInput.textContent = result;
+            }
     }
 };
 
+function populate(str) {
+    if(flag==false){
+        if(displayInput.textContent.length<8){
+            displayInput.textContent = displayInput.textContent.concat(str);
+        }
+    }else {
+        displayLog.textContent = displayInput.textContent + ";  ";
+        displayInput.textContent="";
+        displayInput.textContent = displayInput.textContent.concat(str);
+        flag=false;
+    }
+};
+
+function beginOperation(str){
+    operator = str;
+    flag = false;
+    firstNumber = parseFloat(displayInput.textContent);
+    // displayLog.textContent = displayLog.textContent + displayInput.textContent.concat(str);
+    displayInput.textContent="";
+};
+
+function clear() {
+    displayLog.textContent="";
+    operator="";
+    firstNumber=0;
+    secondNumber=0;
+    flag=false;
+};
+
+let firstNumber;
+let secondNumber;
+let operator;     // Should accept +, -, *, /
+let flag = false;
+
+let displayInput = document.querySelector(".displayInput");
+let displayLog = document.querySelector(".displayLog");
+
+const digits = document.querySelectorAll(".digit");  //Selects all digits in div.digits and "."
+digits.forEach((digit) => {digit.addEventListener("click", () => populate(digit.textContent))})
+
+const operators = document.querySelectorAll(".operator");  //Selects all operators
+operators.forEach((symbol) => {symbol.addEventListener("click", () => beginOperation(symbol.textContent))});
+
+const result = document.querySelector(".result");  //Selects the "=" button
+result.addEventListener("click", () => operate(firstNumber, secondNumber, operator));
+
+const AC = document.querySelector(".ac");       //Selects the "AC" button
+AC.addEventListener("click", () => clear());
